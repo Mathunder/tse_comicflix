6536ac79f37d369aa2c2283ced547ed26295db61
Thomas PEZDA <thomas.pezda@etu.univ-telecom-st-etienne.fr>
2022-10-25 18:43:27 +0200

    test number 2

--- dev/src/main/java/dev/thomasHello.java
+++ dev/src/main/java/dev/thomasHello.java
@@ -2,6 +2,6 @@ package dev;
 
 public class thomasHello {
 	public static void main(String[] args) {
-		System.out.println("Hello World by Thomas !");
+		System.out.println("Hello World by Thomas number 2 !");
 	}
 }
