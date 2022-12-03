4dc2d75f7efc1852e6d5a709bf24b2a5514b5d95
axelantoine <ax.antoine42@gmail.com>
2022-12-03 16:01:08 +0100

    Adding default button component class and use it in login form

--- dev/src/main/java/app/ui/frames/LoginForm.java
+++ dev/src/main/java/app/ui/frames/LoginForm.java
@@ -14,14 +14,14 @@ import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.SpringLayout;
 import java.awt.Toolkit;
+import app.ui.components.DefaultButton;
+import app.ui.themes.CustomColor;
 
 public class LoginForm extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtField_username;
 	private JTextField txtField_password;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
 
 	/**
 	 * Launch the application.
@@ -58,54 +58,50 @@ public class LoginForm extends JFrame {
 		
 		JLabel lblUsername = new JLabel("Username");
 		sl_contentPane.putConstraint(SpringLayout.NORTH, lblUsername, 0, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblUsername, 45, SpringLayout.WEST, contentPane);
 		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblUsername, 54, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, lblUsername, 146, SpringLayout.WEST, contentPane);
-		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 11));
+		sl_contentPane.putConstraint(SpringLayout.EAST, lblUsername, -282, SpringLayout.EAST, contentPane);
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblUsername.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblUsername);
 		
 		txtField_username = new JTextField();
-		sl_contentPane.putConstraint(SpringLayout.NORTH, txtField_username, 11, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, txtField_username, 204, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, txtField_username, -10, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, txtField_username, 13, SpringLayout.NORTH, lblUsername);
+		sl_contentPane.putConstraint(SpringLayout.EAST, txtField_username, -34, SpringLayout.EAST, contentPane);
 		contentPane.add(txtField_username);
 		txtField_username.setColumns(1);
 		
 		JLabel lblPassword = new JLabel("Password");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, lblPassword, 54, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblPassword, 0, SpringLayout.SOUTH, lblUsername);
 		sl_contentPane.putConstraint(SpringLayout.WEST, lblPassword, 45, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, lblPassword, 0, SpringLayout.EAST, lblUsername);
-		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 11));
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblPassword, -147, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, lblPassword, -282, SpringLayout.EAST, contentPane);
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblPassword);
 		
 		txtField_password = new JTextField();
-		sl_contentPane.putConstraint(SpringLayout.WEST, txtField_password, 58, SpringLayout.EAST, lblPassword);
-		sl_contentPane.putConstraint(SpringLayout.EAST, txtField_password, -10, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, txtField_password, 180, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, txtField_username, 0, SpringLayout.WEST, txtField_password);
 		sl_contentPane.putConstraint(SpringLayout.SOUTH, txtField_username, -22, SpringLayout.NORTH, txtField_password);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, txtField_password, 11, SpringLayout.NORTH, lblPassword);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, txtField_password, 13, SpringLayout.NORTH, lblPassword);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, txtField_password, -156, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, txtField_password, -34, SpringLayout.EAST, contentPane);
 		contentPane.add(txtField_password);
 		txtField_password.setColumns(1);
 		
-		btnNewButton = new JButton("Cancel");
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblPassword, -67, SpringLayout.NORTH, btnNewButton);
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblUsername, 0, SpringLayout.WEST, btnNewButton);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnNewButton, 175, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnNewButton, 45, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnNewButton, 214, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnNewButton, 164, SpringLayout.WEST, contentPane);
-		btnNewButton.setForeground(new Color(0, 0, 0));
-		btnNewButton.setFont(new Font("Tahoma", Font.ITALIC, 14));
-		contentPane.add(btnNewButton);
+		DefaultButton btnLogin = new DefaultButton("Login", CustomColor.Red, 14, true);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnLogin, 52, SpringLayout.SOUTH, txtField_password);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnLogin, 230, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnLogin, -60, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnLogin, -79, SpringLayout.EAST, contentPane);
+		contentPane.add(btnLogin);
 		
-		btnNewButton_1 = new JButton("Login");
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, txtField_password, -78, SpringLayout.NORTH, btnNewButton_1);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnNewButton_1, 175, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnNewButton_1, 258, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnNewButton_1, 214, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnNewButton_1, 377, SpringLayout.WEST, contentPane);
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		contentPane.add(btnNewButton_1);
+		DefaultButton btnCancel = new DefaultButton("Cancel", CustomColor.Gray, 14, true);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnCancel, 0, SpringLayout.NORTH, btnLogin);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnCancel, 66, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnCancel, -60, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnCancel, -50, SpringLayout.WEST, btnLogin);
+		contentPane.add(btnCancel);
 	}
-
 }
