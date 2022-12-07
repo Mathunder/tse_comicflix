967bd8595a5760414f6908b895844ab628123b7e
RedRosh <61188411+RedRosh@users.noreply.github.com>
2022-12-07 13:07:30 +0100

    Async Calls Done

--- dev/src/main/java/app/Main.java
+++ dev/src/main/java/app/Main.java
@@ -1,18 +1,14 @@
 package app;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import app.dto.SearchResultDto;
-import app.helpers.ComicVineSearchFilter;
-import app.services.ComicVineService;
-import app.ui.frames.*;
+import app.ui.frames.MainFrame;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		new MainFrame();
+
+
+
 	}
 
 }
-	
--- dev/src/main/java/app/services/ComicVineService.java
+++ dev/src/main/java/app/services/ComicVineService.java
@@ -23,8 +23,8 @@ public class ComicVineService {
 		RestAssured.baseURI = "https://comicvine.gamespot.com/api";
 		RestAssured.port = 443;
 	}
-
-	public SearchResultDto search(String keyword, List<ComicVineSearchFilter> filters, int limit, int offset) {
+	
+	public SearchResultDto  search(String keyword, List<ComicVineSearchFilter> filters, int limit, int offset) {
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("api_key", "f9073eee3658e2a4f39a9f531ad521b935ce87bc");
 		params.put("resources",
@@ -36,7 +36,7 @@ public class ComicVineService {
 		String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0";
 		SearchResultDto result = given().params(params).header("User-Agent", userAgent).expect().statusCode(200)
 				.body("status_code", equalTo(1)).when().get("/search").as(SearchResultDto.class);
-		return result;
+		return (result);
 	}
 
 }
