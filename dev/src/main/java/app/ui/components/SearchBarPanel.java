d3452ae9c4b56b8ce29fecf4af1ce3859b0ecb4c
axelantoine <ax.antoine42@gmail.com>
2022-12-07 14:28:33 +0100

    Fixing layout for searchBar panel in order to prevent visualisation problems

--- dev/src/main/java/app/ui/components/SearchBarPanel.java
+++ dev/src/main/java/app/ui/components/SearchBarPanel.java
@@ -20,26 +20,40 @@ import app.helpers.ComicVineSearchFilter;
 import app.services.ComicVineService;
 import app.ui.frames.MainFrame;
 import app.ui.themes.CustomColor;
+import javax.swing.SpringLayout;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
 
 public class SearchBarPanel extends JPanel {
-	
-	private SearchBar searchRoundBar;
 	private DefaultButton btnFilter;
 	private SearchResultDto result;
+	private SearchBar searchRoundBar;
 	
 	public SearchBarPanel(){
-		searchRoundBar = new SearchBar();
+		setBackground(new Color(249, 246, 241));
 		initButtonSearch();
-		
-		this.add(searchRoundBar);
+		SpringLayout springLayout = new SpringLayout();
+		springLayout.putConstraint(SpringLayout.NORTH, btnFilter, 85, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, btnFilter, 550, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnFilter, -25, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnFilter, -550, SpringLayout.EAST, this);
+		setLayout(springLayout);
 		this.add(btnFilter);
-		this.setLayout(new GridLayout(0,2));
+		
+		searchRoundBar = new SearchBar();
+		searchRoundBar.setSearchText("Search");
+		springLayout.putConstraint(SpringLayout.NORTH, searchRoundBar, 25, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, searchRoundBar, 400, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, searchRoundBar, -50, SpringLayout.SOUTH, btnFilter);
+		springLayout.putConstraint(SpringLayout.EAST, searchRoundBar, -400, SpringLayout.EAST, this);
+		add(searchRoundBar);
 
 	}
 	
 	//Init button search
 	private void initButtonSearch() {
 		btnFilter = new DefaultButton(" Search ", CustomColor.Red, 20, true);
+		btnFilter.setText("Go");
 			
 		btnFilter.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
@@ -51,12 +65,13 @@ public class SearchBarPanel extends JPanel {
 	//Action button search
 	private void btnFilterActionPerformed(ActionEvent evt) {
 
-		if(searchRoundBar.getSearchText()!="") {
+		if(searchRoundBar.getSearchText()!="" && searchRoundBar.getSearchText() != "Search" && searchRoundBar.getSearchText() != "Loading...") {
 			ComicVineService comicVineService = new ComicVineService();
 			List<ComicVineSearchFilter> filters = new ArrayList<>();
 			//filters.add(ComicVineSearchFilter.ISSUE);
 	        filters.add(ComicVineSearchFilter.CHARACTER);
-			String keyword = searchRoundBar.getSearchText();
+			String keyword = searchRoundBar.getSearchText().replaceAll(" ", "-");
+			System.out.println(keyword);
 			CompletableFuture.runAsync(() -> {
 				System.out.println();
 				SearchResultDto AsyncResults = comicVineService.search(keyword, filters, 16, 0);
@@ -79,7 +94,4 @@ public class SearchBarPanel extends JPanel {
 	public void setResult(SearchResultDto result) {
 		this.result = result;
 	}
-	
-	
-	
 }
