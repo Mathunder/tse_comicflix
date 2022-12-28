4a0e07e00f5b229e7fbb762cc422c58136bd800c
dhoop.matthieu <matthieu.dhoop@gmail.com>
2022-12-28 16:26:10 +0100

    updating branch onto develop

--- dev/src/main/java/app/entities/Issue.java
+++ dev/src/main/java/app/entities/Issue.java
@@ -1,9 +1,9 @@
 package app.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.List;
 
 import app.dto.ImageResultDto;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
@@ -15,8 +15,16 @@ public class Issue {
 	private String issue_number;
 	private String name;
 	private ImageResultDto image;
-	//private String publisher;
-	private String description;
-//	private List<Creators> person_credits;
-//	private List<Character> character_credits;
+	
+	public Issue() {}
+	
+	public Issue(String aliases, String api_detail_url, int id, String issue_number, String name, String image_url) {
+		this.aliases=aliases;
+		this.api_detail_url = api_detail_url;
+		this.id = id;
+		this.issue_number = issue_number;
+		this.name = name;
+		this.image = new ImageResultDto(image_url);
+	}
+
 }
