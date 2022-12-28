55da4eb219f4df8df3aca53ec6cae2bd03468dcc
dhoop.matthieu <matthieu.dhoop@gmail.com>
2022-12-28 16:27:04 +0100

    Updating branch onto develop

--- dev/src/main/java/app/entities/Comics.java
+++ dev/src/main/java/app/entities/Comics.java
@@ -1,81 +1,35 @@
 package app.entities;
 
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
-
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-
-import app.ui.components.ComicsInfosPanel;
-import app.ui.frames.MainFrame;
-
-// The comic class has two objectives : 
-// 1. To provide names & pictures for the comics that are to be displayed when a research is made by the user
-// 2. To provide name, picture, synopsis and all the informations that are to be displayed when the user clicks on a comic
 
 public class Comics {
 	private String title;
 	private ImageIcon image;
-	private URL url;
-	private String synopsis;
-	private List<String> creators;
-	private List<String> characters;
+	private String ID;
 	
-	//Constructor 
-	public Comics(Issue issue) {
-		// Initialization of the comic's name from issue.getName()
-		this.title = issue.getName();
-		
-		// Initialization of the comic's image from issue.getImage()
-		try {
-			URL url = new URL(issue.getImage().getOriginal_url());
-			BufferedImage imageBrute = ImageIO.read(url);
-			Image imageResize = imageBrute.getScaledInstance(206, 310, Image.SCALE_DEFAULT);
-			this.image = new ImageIcon(imageResize);
-			
-
-		} catch (IOException e) {
-			System.out.println("Problem load img");
-			e.printStackTrace();
-		}
-		
-		// Initialization of the description
-		this.synopsis = issue.getDescription();
-		
-//		// Initialization of the creators
-//		for (int i = 0; i < issue.getPerson_credits().size(); i++) {
-//			this.creators.add("Name : " + issue.getPerson_credits().get(i).getName());
-//			this.creators.add("Role : " + issue.getPerson_credits().get(i).getRole());
-//		}
-//		
-//		for (int i = 0; i < issue.getCharacter_credits().size(); i++) {
-//			this.creators.add("Name : " + issue.getCharacter_credits().get(i).getName());
-//			// if image
-//			// this.creators.add("Role : " + issue.getCharacter_credits().get(i).getImage());
-//			// or if the image is a url, do like above
-//		}
+	//Constructor
+	public Comics(String title, ImageIcon image2) {
+		this.title = title;
+		this.image = image2;
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 	public ImageIcon getImage() {
 		return this.image;
 	}
-	
-	public String getSynopsis() {
-		return this.synopsis;
+
+	public void setImage(ImageIcon image) {
+		this.image = image;
 	}
 	
-	public List<String> getCreators() {
-		return this.creators;
-	}
-	
-	public List<String> getCharacters() {
-		return this.characters;
+	public String getID() {
+		return this.ID;
 	}
 }
