0a249c26df7cf4a465fbdd25299c562cebd13c8b
dhoop.matthieu <matthieu.dhoop@gmail.com>
2022-12-29 22:18:46 +0100

    removing the comic class

--- dev/src/main/java/app/ui/components/LabelComics.java
+++ dev/src/main/java/app/ui/components/LabelComics.java
@@ -1,22 +1,24 @@
-package app.ui.components;
-
-import java.awt.Font;
-import javax.swing.JLabel;
-import app.entities.Comics;
-import app.ui.themes.CustomColor;
-import java.awt.event.*;
-
-public class LabelComics extends JLabel{
-	
-	LabelComics(Comics comics){
-				
-		super(comics.getTitle(), comics.getImage(), JLabel.CENTER); //Call constructor of the mother class (JLabel)
-				
-		this.setForeground(CustomColor.Black);
-		this.setFont(new Font("Arial", Font.PLAIN, 20)); //set font of text
-		this.setBackground(CustomColor.WhiteCloud);
-		this.setHorizontalTextPosition(JLabel.CENTER); //set text L, R, CENTER of imageIcon
-		this.setVerticalTextPosition(JLabel.BOTTOM);
-	}
-}
-
+//package app.ui.components;
+//
+//import java.awt.Font;
+//
+//import javax.swing.ImageIcon;
+//import javax.swing.JLabel;
+//import app.entities.ResultsAPI;
+//import app.ui.themes.CustomColor;
+//import java.awt.event.*;
+//
+//public class LabelComics extends JLabel{
+//	
+//	LabelComics(ResultsAPI results_api, ImageIcon img){
+//				
+//		super(results_api.getName(), img, JLabel.CENTER); //Call constructor of the mother class (JLabel)
+//				
+//		this.setForeground(CustomColor.Black);
+//		this.setFont(new Font("Arial", Font.PLAIN, 20)); //set font of text
+//		this.setBackground(CustomColor.WhiteCloud);
+//		this.setHorizontalTextPosition(JLabel.CENTER); //set text L, R, CENTER of imageIcon
+//		this.setVerticalTextPosition(JLabel.BOTTOM);
+//	}
+//}
+//
