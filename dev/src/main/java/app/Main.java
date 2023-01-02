49b63e21a0f8f7b2320fa6717f469e095c6d0b3e
RedRosh <61188411+RedRosh@users.noreply.github.com>
2023-01-02 17:55:34 +0100

    clean code

--- dev/src/main/java/app/Main.java
+++ dev/src/main/java/app/Main.java
@@ -9,11 +9,9 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		// Models
 		UserModel userModel = new UserModel();
-
 		// Controller
 		ComicVineService comicVineService = new ComicVineService();
 		DatabaseService databaseService = new DatabaseService(userModel);
-
 		new MainFrame(userModel, comicVineService, databaseService);
 
 	}
